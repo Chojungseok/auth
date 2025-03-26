29816ef4f9936c64809b597e5f864e4bcf9ce020
Chojungseok <whwndtjr9712@naver.com>
2025-03-26 14:16:42 +0900

    article modeling/migration

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from accounts.models import User
+from django.conf import settings
+from django.contrib.auth import get_user_model
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    # 직접 참조(비추)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # settings.py 변수 활용
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    # get_user_model
+    # user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
