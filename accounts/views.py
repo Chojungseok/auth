57d05debcc2232527772031c4d1b1df35967fa65
Chojungseok <whwndtjr9712@naver.com>
2025-03-26 11:44:44 +0900

    signup func 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 # Create your views here.
+def signup(request):
+    if request.method == 'POST' :
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            # hash함수: 어떠한 글자를 넣으면 그 글자를 특정글자로 바꿔주는 함수이다.
+            form.save()
+            return redirect('accounts:login')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'signup.html', context)
