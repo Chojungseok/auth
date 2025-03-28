9dd019ce569a23a27c24dcc98087acde194aea82
Chojungseok <whwndtjr9712@naver.com>
2025-03-28 09:48:03 +0900

    Comment modeling/migration

--- articles/models.py
+++ articles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from accounts.models import User
 from django.conf import settings
 from django.contrib.auth import get_user_model
+
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -16,3 +17,8 @@ class Article(models.Model):
     # get_user_model
     # user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
+
+class Comment(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
